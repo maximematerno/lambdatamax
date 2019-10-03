d37f0a1fa68f5c3c86f7450326ee7087e8181a5a
maxime.materno@gmail.com <maxime.materno@gmail.com>
2019-10-03 11:46:24 -0700

    init setup

---
+++ setup.py
@@ -0,0 +1,35 @@
+"""
+lambdatamax - A Collection of Data Science helper functions.
+"""
+
+
+import setuptools
+
+REQUIRED = [
+    "numpy",
+    "pandas"
+]
+
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+setuptools.setup(
+    name="lambdatamax",
+    version="0.0.3",
+    author="maximematerno",
+    description="A Collection of Data Science helper functions",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://github.com/maximematerno/lambdatamax",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.5",
+    install_required=REQUIRED,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+
+
+
+)
