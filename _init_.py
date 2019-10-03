da95d3e87d9704bf8053e6b2cb971bb07999875f
maxime.materno@gmail.com <maxime.materno@gmail.com>
2019-10-03 13:01:29 -0700

    split

--- _init_.py
+++ _init_.py
@@ -12,19 +12,3 @@ ONES = np.ones(10)
 
 
 
-def split(X,y):
-
-    train_size= .8
-    val_size= .1
-    test_size=.1
-    
-    assert train_size + val_size + test_size == 1
-
-    X_train_val, X_test, y_train_val, y_test = train_test_split(
-    X, y, test_size= test_size, random_state=random_state, shuffle=shuffle)
-
-    X_train, X_val, y_train, y_val = train_test_split(
-    X_train_val, y_train_val, test_size= val_size/(train_size+val_size),
-    random_state= random_state, shuffle=shuffle)
-
-    return X_train, X_val, X_test, y_train, y_val, y_test
